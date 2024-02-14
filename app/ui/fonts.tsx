762b9de8f11a6aa9071cf9be42fbd5a4ad0e1235
Kittitra <kittra0078@gmail.com>
2024-02-14 15:24:18 +0700

    first commit

---
+++ app/ui/fonts.tsx
@@ -0,0 +1,5 @@
+import { Inter, Roboto } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+ 
+export const inter = Roboto({ weight: '300', subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: '400', subsets: ['latin'] })
